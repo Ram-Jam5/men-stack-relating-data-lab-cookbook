71131eb272c3c0bb9fdcd8b74a13445ca2ec0570
Ram-Jam5 <edgeillusive@gmail.com>
2024-07-03 17:06:42 -0500

    create food controller. build index view and functionality for adding new foods. User log in and auth implemented.

--- controllers/foods.js
+++ controllers/foods.js
@@ -5,33 +5,37 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {    
-        res.render('foods/index.ejs')
+        const currentUser = await User.findById(req.session.user._id);
+        res.render('foods/index.ejs', {
+            pantry: currentUser.pantry,
+        });
     } catch (error) {
         console.log(error);
         res.redirect('/')
     }
 });
 
-// router.get('/new', async (req, res) => {
-//     try {
-//         res.render('foods/new.ejs');
-//     } catch (error) {
-//         console.log(error);
-//         res.redirect('/')
-//     }
-// });
+router.get('/new', async (req, res) => {
+    try {
+        res.render('foods/new.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
+});
 
-// router.post('/', async (req, res) => {
-//     try {
-//         const currentUser = await User.findById(req.session.user._id);
-//         currentUser.foods.push(req.body);
-//         await currentUser.save();
-//         res.redirect(`/users/${currentUser._id}/foods`);
-//     } catch (error) {
-//         console.log(error);
-//         res.redirect('/')
-//     }
-// })
+router.post('/',async (req, res) => {
+    console.log(req.body)
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/')
+    }
+})
 
 module.exports = router;
 
